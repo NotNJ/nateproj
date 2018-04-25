cae6bde02c2e4730fbb1628e37e9bca69b6cebe4
Unknown <zach@30lines.com>
2018-04-25 19:35:32 -0400

    # Nav now uses bootstrap's grids

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,25 +1,33 @@
 <nav class="main-nav container-fluid">
 	<ul class="primary-nav row justify-content-between">
-		<div class="col-2">
+		<div class="col-1">
 			<li class="logo">
 				<a href="/" class="image-link"><img src="https://i.imgur.com/H1eDQhG.png" alt="NJ"></a>
 			</li>
 		</div>
-		<div class="col-5">
-			<li class="small-50">
-				<a href="/store" class="normal-link">Store</a>
-			</li>
-			<li class="small-50">
-				<a href="/about" class="normal-link">About Us</a>
-			</li>
-			<li class="small-50">
-				<a href="/test" class="normal-link">Newsletter</a>
-				<div class="col-4">
+		<div class="col-4">
+			<div class="row justify-content-center">
+				<div class="col-3">
+					<li class="small-50">
+					<a href="/store" class="normal-link">Store</a>
+				</li>
+				</div>
+				<div class="col-3">
+					<li class="small-50">
+						<a href="/about" class="normal-link">About Us</a>
+					</li>
+				</div>
+				<div class="col-3">
+					<li class="small-50">
+						<a href="/test" class="normal-link">Newsletter</a>
+					</li>
+				</div>
+				<div class="col-3">
 					<li class="small-50">
 						<a href="/images" class="normal-link">Images</a>
 					</li>
 				</div>
-			</li>
+			</div>
 		</div>
 	</ul>
 </nav>
