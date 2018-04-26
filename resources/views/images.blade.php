2f06fedc36acb32d5aad15e5c38c65050e90b2d7
Unknown <natejackemeyer@gmail.com>
2018-04-25 20:56:39 -0400

    Merge branch 'nav_edits' into develop

--- resources/views/images.blade.php
+++ resources/views/images.blade.php
@@ -4,8 +4,37 @@
 @include('layouts.header2')
 @include('nav')
     <body>
-        <div>
-          <p>Coming Soon!</p>
+        <div class="container">
+			<div class="row">
+				<div class="col-6">
+          			<p>
+          				Duis elementum, metus sit amet dictum sodales, elit est sagittis mauris, id vestibulum libero mauris quis velit. Proin vel turpis dapibus, ullamcorper leo quis, fringilla nunc. Mauris ac augue sem. Maecenas nec diam volutpat, euismod est congue, fermentum tellus.Duis elementum, metus sit amet dictum sodales, elit est sagittis mauris, id vestibulum libero mauris quis velit. Proin vel turpis dapibus, ullamcorper leo quis, fringilla nunc. Mauris ac augue sem. Maecenas nec diam volutpat, euismod est congue.
+          			</p>
+          		</div>
+          		<div class="col-6">
+				 	<div class="polaroid">
+				 		<img src="https://goo.gl/DS2o7e" alt="landscape" style="width:540px">
+						<div class="polaroid-container">
+				  			<p>The text that goes below the image"</p>
+				  		</div>
+				  	</div>
+          		</div>
+				<div class="row">
+					<div class="col-6">
+						<p>
+							Duis elementum, metus sit amet dictum sodales, elit est sagittis mauris, id vestibulum libero mauris quis velit. Proin vel turpis dapibus, ullamcorper leo quis, fringilla nunc. Mauris ac augue sem. Maecenas nec diam volutpat, euismod est congue, fermentum tellus.Duis elementum, metus sit amet dictum sodales, elit est sagittis mauris, id vestibulum libero mauris quis velit. Proin vel turpis dapibus, ullamcorper leo quis, fringilla nunc. Mauris ac augue sem. Maecenas nec diam volutpat, euismod est congue.
+						</p>
+					</div>
+					<div class="col-6">
+					 	<div class="polaroid">				 		
+					 		<img src="https://img00.deviantart.net/ea23/i/2014/201/c/7/random_landscape_by_jalv-d7rkaif.jpg" alt="landscape2" style="width:540px">
+							<div class="polaroid-text">
+					  			<p>The text that goes below the image"</p>
+					  		</div>
+					  	</div>
+	          		</div>
+          		</div>
+			</div>
         </div>
     </body>
 </html>
